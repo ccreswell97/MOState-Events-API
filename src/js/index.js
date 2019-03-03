a2b8802b9b79ea7d7dcdc1a8622a177d17e9b7ef
Carmen Creswell <ccreswell@adaptavist.com>
2019-03-03 13:34:24 -0600

    Implement error handling, implement loading gif

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,26 @@
 'use strict';
 
-const ul = document.getElementById('events'); 
-const url = 'http://people.missouristate.edu/chadkillingsworth/csc590/calendar/?date=02/14/2019'; 
-
+const ul = document.getElementById('events');
+let date = '02/14/2019' 
+const url = 'http://people.missouristate.edu/chadkillingsworth/csc590/calendar/?date=' + date; 
+let loadingImg = document.getElementById("loader");
 
 fetch(url)
-.then((resp) => resp.json())
+.then(function(response) {
+    let status = response.status;
+    console.log(status);
+
+    if (!response.ok) {
+        document.getElementById("errorHolder").innerHTML = status + " " + response.statusText + ". Refresh to continue";
+    }
+
+    return response.json();
+})
 .then(function(data) {
     console.log(data);
+    
+    loadingImg.style.display='none';
+    
     for (let i = 0; i < data.feed.entry.length; i++) {
         let event = data.feed.entry[i].title.$t; 
         let li = document.createElement('li');
@@ -16,5 +29,6 @@ fetch(url)
     }
 })
 .catch(function(error) {
+    loadingImg.style.display='none';
     console.log(error);
 });
